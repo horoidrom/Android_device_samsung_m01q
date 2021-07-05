9b58ad1ffb2809104ba27c1586c782fc6fa7935b
Vergil Pampanga <83948563+mastersenpai05@users.noreply.github.com>
2021-07-05 15:55:29 +0800

    Update omni_m01q.mk

--- omni_m01q.mk
+++ omni_m01q.mk
@@ -24,15 +24,10 @@ PRODUCT_USE_DYNAMIC_PARTITION := tru
 
 # Inherit from those products. Most specific first.
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from m01q device
-$(call inherit-product, device/samsung/m01q/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m01q
